5221055f5e28ab7473ea4b03e115d2d09512c7c6
Lucas <lucashpsfei@gmail.com>
2019-02-24 22:18:15 -0300

    Versão 1.0 - Cenário, player e laterna feitos

---
+++ Projeto_Fear_Game/Assets/SCRIPTS/Lanterna.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lanterna : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f"))
+        {
+            if(GetComponent<Light>().enabled == true)
+            {
+                GetComponent<Light>().enabled = false;
+            }
+            else if(GetComponent<Light>().enabled == false)
+            {
+                GetComponent<Light>().enabled = true;
+            }
+                
+        }
+    }
+
+   
+
+}
